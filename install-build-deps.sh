128cc656dda007a0cf25ce58cef83318c05b4247
Philipp Wagner <mail@philipp-wagner.com>
2017-06-09 21:54:21 +0200

    add travis support

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -5,10 +5,14 @@
 
 case $(lsb_release -is) in
   Ubuntu)
+    apt-get install \
+      check \
+      doxygen python3 python3-venv python3-pip \
+      lcov
     ;;
 
   *SUSE*)
-    sudo zypper install \
+    zypper install \
       libcheck0 check-devel \
       doxygen python3 python3-pip \
       lcov
